f83f016b08ce534ee0898c04aa7ecbdc3c26de76
Maxim Savin <soulman.is.good@gmail.com>
2015-04-30 15:28:58 +0600

    from url upload with redirection handling

--- routes/url.js
+++ routes/url.js
@@ -5,25 +5,37 @@ exports.get = function (req, res, next) {
   //Download file from url
   var url = req.params.url;
 
-  if (url) {
-    url = decodeURIComponent(url);
-    console.log("Downloading from url: '" + url + "'");
-    if (url.indexOf('https:') === 0) {
-      protocol = require('https');
+  var maxJumps = 10;
+  function findFile (url) {
+    if (url) {
+      url = decodeURIComponent(url);
+      console.log("Downloading from url: '" + url + "'");
+      if (url.indexOf('https:') === 0) {
+        protocol = require('https');
+      }
+    }
+    if (url && maxJumps-- > 0) {
+      var request = protocol.get(url, function (response) {
+        if (response.statusCode >= 300 && response.statusCode < 400) {
+          findFile(response.headers['location']);
+          request.end();
+        } else {
+          req.storeFile(response, res.doneFileDownload.bind(res));
+        }
+      });
+      request.on('error', function (err) {
+        next(err);
+      });
+      request.on('timeout', function () {
+        var err = new Error("Request timeout");
+        err.code = 408;
+        next(err);
+        request.abort();
+      });
+      request.setTimeout(15000);
+    } else {
+      res.json({error: maxJumps>0?'Wrong location':'Redirect limit reached'});
     }
   }
-  var request = protocol.get(url, function (response) {
-    req.storeFile(response, res.doneFileDownload.bind(res));
-  });
-  request.on('error', function (err) {
-    next(err);
-  });
-  request.on('timeout', function () {
-    var err = new Error("Request timeout");
-    err.code = 408;
-    next(err);
-    request.abort();
-  });
-  request.setTimeout(15000);
-
+  findFile(url);
 };
